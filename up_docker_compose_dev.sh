39fca9fb3eb2a1dc56ed908f0bd207587d0e098a
nikwork <vlasov.bis@gmail.com>
2022-08-15 23:26:03 +0300

    Add top-level diagram

--- up_docker_compose_dev.sh
+++ up_docker_compose_dev.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+# Show env vars
+echo Show env vars:
+echo
+grep -v '^#' ./config/.env.dev
+echo
+
 export NEO4J_VERSION="latest"
+
+echo Docker-compose UP:
+echo
 docker-compose --env-file ./config/.env.dev up
